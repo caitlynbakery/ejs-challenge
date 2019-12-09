856a67451fdae4e27867aeec0e23c832f29113d9
Caitlyn <caitlynbakery@email.com>
2019-12-09 07:15:38 -0800

    finished video 262

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res) {
-  res.render("home", {homeContentEJS: homeStartingContent});
+  res.render("home", {
+    homeContentEJS: homeStartingContent, 
+    postsEJS: posts});
+
 });
 
 app.get("/contact", function(req, res) {
@@ -32,7 +37,13 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+  res.redirect("/");
 
 });
 
